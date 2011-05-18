c6dc2ef39936e23d4babc31a73a67705cc7b59e4
Your Name <you@email.com>
2011-05-18 10:05:14 -0600

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/IProcessIncomingRequests.cs
+++ source/nothinbutdotnetstore/web/core/IProcessIncomingRequests.cs
@@ -1,7 +1,18 @@
+using System;
+
 namespace nothinbutdotnetstore.web.core
 {
   public interface IProcessIncomingRequests
   {
     void process(object request);
   }
+
+    public class FrontController : IProcessIncomingRequests
+
+    {
+        public void process(object request)
+        {
+            throw new NotImplementedException();
+        }
+    }
 }
--- source/nothinbutdotnetstore/web/core/RawHandler.cs
+++ source/nothinbutdotnetstore/web/core/RawHandler.cs
@@ -5,9 +5,18 @@ namespace nothinbutdotnetstore.web.core
 {
   public class RawHandler : IHttpHandler
   {
-    public void ProcessRequest(HttpContext context)
+      private readonly IProcessIncomingRequests _processIncomingRequest;
+      private readonly ICreateRequests _createRequests;
+
+      public RawHandler(IProcessIncomingRequests processIncomingRequest, ICreateRequests createRequests)
+      {
+          _processIncomingRequest = processIncomingRequest;
+          _createRequests = createRequests;
+      }
+
+      public void ProcessRequest(HttpContext context)
     {
-      throw new NotImplementedException();
+      _processIncomingRequest.process(_createRequests.create_request_from(context));
     }
 
     public bool IsReusable
