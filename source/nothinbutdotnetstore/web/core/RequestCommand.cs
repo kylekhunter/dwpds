15300bc5b977d5d3106366fda989976d2c6d666d
Jonathan Dennis <dennisj@geoprobe.com>
2011-05-18 11:46:03 -0600

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/RequestCommand.cs
+++ source/nothinbutdotnetstore/web/core/RequestCommand.cs
@@ -5,15 +5,17 @@ namespace nothinbutdotnetstore.web.core
   public class RequestCommand : IProcessRequestInformation
   {
       private readonly RequestCriteria request_criteria;
+      private readonly IProcessAnApplicationSpecificBehaviour behavior;
 
-      public RequestCommand(RequestCriteria request_criteria)
+      public RequestCommand(RequestCriteria request_criteria, IProcessAnApplicationSpecificBehaviour behavior)
       {
           this.request_criteria = request_criteria;
+          this.behavior = behavior;
       }
 
       public void run(IContainRequestInformation request)
     {
-      throw new NotImplementedException();
+         behavior.run(request);
     }
 
     public bool can_process(IContainRequestInformation request)
