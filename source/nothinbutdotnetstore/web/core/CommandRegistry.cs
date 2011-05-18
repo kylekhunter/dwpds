a370d8440b08e5c6fb6257a899af5017facfa805
mkmurray <charliebrown928@hotmail.com>
2011-05-18 11:12:09 -0600

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/CommandRegistry.cs
+++ source/nothinbutdotnetstore/web/core/CommandRegistry.cs
@@ -6,15 +6,18 @@ namespace nothinbutdotnetstore.web.core
   public class CommandRegistry : IFindCommands
   {
     IEnumerable<IProcessRequestInformation> process_request_commands;
+    MissingCommandFactory missing_command_factory;
 
-    public CommandRegistry(IEnumerable<IProcessRequestInformation> process_request_commands)
+    public CommandRegistry(IEnumerable<IProcessRequestInformation> process_request_commands, MissingCommandFactory missing_command_factory)
     {
       this.process_request_commands = process_request_commands;
+      this.missing_command_factory = missing_command_factory;
     }
 
     public IProcessRequestInformation get_the_command_that_can_process(IContainRequestInformation request)
     {
-      return process_request_commands.First(x => x.can_process(request));
+      var result =  process_request_commands.FirstOrDefault(x => x.can_process(request));
+      return result ?? missing_command_factory();
     }
   }
 }
