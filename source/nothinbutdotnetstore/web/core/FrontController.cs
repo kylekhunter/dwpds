cfe9b8d6fea73432d2d676c5755a57f1768e41a4
Kevin Krueger <kevin.krueger@gmail.com>
2011-05-18 10:43:33 -0600

    Pushing new changes

--- source/nothinbutdotnetstore/web/core/FrontController.cs
+++ source/nothinbutdotnetstore/web/core/FrontController.cs
@@ -4,9 +4,16 @@ namespace nothinbutdotnetstore.web.core
 {
   public class FrontController : IProcessIncomingRequests
   {
+    private IFindCommands command_registry;
+
+    public FrontController(IFindCommands command_registry)
+    {
+      this.command_registry = command_registry;
+    }
     public void process(IContainRequestInformation request)
     {
-      throw new NotImplementedException();
+      var command = command_registry.get_the_command_that_can_process(request);
+      command.run(request);
     }
   }
 }
