679638490c0a31c94b9a5bc00a459121594f7641
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-05-17 19:59:57 -0600

    Updated

--- source/nothinbutdotnetstore.specs/CalculatorSpecs.cs
+++ source/nothinbutdotnetstore.specs/CalculatorSpecs.cs
@@ -15,15 +15,30 @@ namespace nothinbutdotnetstore.specs
 
     public class when_adding_two_positive_numbers : concern
     {
+      Establish c = () =>
+      {
+        connection = depends.on<IDbConnection>();
+        command = fake.an<IDbCommand>();
+
+        connection.setup(x => x.CreateCommand()).Return(command);
+      };
+
       Because b = () =>
         result = sut.add(2, 3);
 
       It should_return_the_sum = () =>
         result.ShouldEqual(5);
 
+      It should_connect_to_the_database = () =>
+        connection.received(x => x.Open());
+
+      It should_run_a_query = () =>
+        command.received(x => x.ExecuteNonQuery());
+
 
       static int result;
       static IDbConnection connection;
+      static IDbCommand command;
     }
 
     public class when_attempting_to_add_a_negative_number : concern
--- source/nothinbutdotnetstore/Calculator.cs
+++ source/nothinbutdotnetstore/Calculator.cs
@@ -1,13 +1,24 @@
 using System;
+using System.Data;
 using System.Linq;
 
 namespace nothinbutdotnetstore
 {
   public class Calculator
   {
+    IDbConnection connection;
+
+    public Calculator(IDbConnection connection)
+    {
+      this.connection = connection;
+    }
+
     public int add(int first_number, int second_number)
     {
       ensure_all_numbers_are_positive(first_number, second_number);
+      connection.Open();
+      connection.CreateCommand().ExecuteNonQuery();
+
       return first_number + second_number;
     }
 
