c098780c07f2023407a868982ba1fffc14d37a2f
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2011-05-18 11:00:34 -0600

    Updated

--- source/nothinbutdotnetstore.specs/CommandRegistrySpecs.cs
+++ source/nothinbutdotnetstore.specs/CommandRegistrySpecs.cs
@@ -13,7 +13,17 @@ namespace nothinbutdotnetstore.specs
     public abstract class concern : Observes<IFindCommands,
                                       CommandRegistry>
     {
-        
+
+      Establish c = () =>
+      {
+        all_possible_commands = Enumerable.Range(1, 100).Select(x => fake.an<IProcessRequestInformation>()).ToList();
+        depends.on<IEnumerable<IProcessRequestInformation>>(all_possible_commands);
+        request = fake.an<IContainRequestInformation>();
+      };
+
+      protected static IList<IProcessRequestInformation> all_possible_commands;
+      protected static IContainRequestInformation request;
+      protected static IProcessRequestInformation result;
     }
 
    
@@ -21,10 +31,6 @@ namespace nothinbutdotnetstore.specs
     {
       Establish c = () =>
       {
-        all_possible_commands = Enumerable.Range(1, 100).Select(x => fake.an<IProcessRequestInformation>()).ToList();
-        depends.on<IEnumerable<IProcessRequestInformation>>(all_possible_commands);
-
-        request = fake.an<IContainRequestInformation>();
         the_command_that_can_process_the_request = fake.an<IProcessRequestInformation>();
 
         all_possible_commands.Add(the_command_that_can_process_the_request);
@@ -41,10 +47,26 @@ namespace nothinbutdotnetstore.specs
       It should_return_the_command_to_the_caller = () =>
         result.ShouldEqual(the_command_that_can_process_the_request);
 
-      static IProcessRequestInformation result;
       static IProcessRequestInformation the_command_that_can_process_the_request;
-      static IContainRequestInformation request;
-      static IList<IProcessRequestInformation> all_possible_commands;
+    }
+
+    public class when_getting_the_command_that_can_process_a_request_and_it_does_not_have_the_command : concern
+    {
+      Establish c = () =>
+      {
+        the_special_case = fake.an<IProcessRequestInformation>();
+        depends.on<MissingCommandFactory>(() => the_special_case);
+      };
+
+      Because b = () =>
+        result = sut.get_the_command_that_can_process(request);
+
+
+      It should_return_the_special_case = () =>
+        result.ShouldEqual(the_special_case);
+
+      static IProcessRequestInformation the_command_that_can_process_the_request;
+      static IProcessRequestInformation the_special_case;
     }
   }
 }
