efeafa94e91e0aacd9df8004de6fcf65ccb63eef
Brad Mead <bradmead@gmail.com>
2011-05-18 17:21:20 -0700

    Pushing new changes

---
+++ source/nothinbutdotnetstore/web/core/RenderDisplay.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace nothinbutdotnetstore.web.core
+{
+  public class RenderDisplay : ICanRenderInformation
+  {
+    public void display<ReportModel>(ReportModel report_model)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
